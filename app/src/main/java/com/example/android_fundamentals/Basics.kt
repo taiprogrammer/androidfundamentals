194d1d95c6ee20bb79786d10eaa7cd24d1156cb8
taiprogrammer <taiza.marques@sptech.school>
2022-12-04 19:45:35 -0300

    variables (val and var)

---
+++ app/src/main/java/com/example/android_fundamentals/Basics.kt
@@ -0,0 +1,12 @@
+package com.example.android_fundamentals
+
+fun main() {
+    /*mutable variable*/
+    var name : String = "John"
+    name = "Doe"
+    println("Hello $name")
+
+    /*immutable variable*/
+    val age : Int = 20
+    println("Your age is: $age")
+}

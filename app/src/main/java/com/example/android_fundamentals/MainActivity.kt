f68f717d3bcecbc2364ec033c6171b1cd0c9dc5f
taiprogrammer <taiza.marques@sptech.school>
2022-12-04 17:17:44 -0300

    testing toast, val and var

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.android_fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.myBtn)
+        val content = findViewById<TextView>(R.id.content)
+        var timeClicked : Int = 0
+
+        btn.setOnClickListener {
+            timeClicked++;
+            content.text = timeClicked.toString()
+
+            val toast = Toast.makeText(this,
+                timeClicked.toString(), Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
